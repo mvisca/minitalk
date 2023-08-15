b17c84237a5b01ee973f2c1c6ad82f1665f2f32f
mvisca <martin.visca@gmail.com>
2023-08-15 17:14:28 +0200

    unicode ok

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:54:35 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/15 13:42:12 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/15 15:13:47 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,25 +41,26 @@ static void	mt_send_signal(pid_t pid, char *str)
 			c = str[i];
 			c = c >> octet--;
 			bit = c & 1;
-			ft_printf("bit %d\n", bit);
 			if (bit == 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(150);
+			usleep(400);
+			if (i % 1500 == 0)
+				usleep(8000);
 		}
+		write (1, "sending... ", 11);
+		write (1, &str[i], 1);
+		write (1, "\n", 1);
 		i++;
-		ft_printf("%c %d\n", c, (int)c);
 	}
 	octet = 8;
 	while (octet > 0)
 	{
-		ft_printf("bit %d\n", 0);
 		kill(pid, SIGUSR2);
-		usleep(150);
+		usleep(100);
 		octet--;
 	}
-	ft_printf("\'\\n\'");
 }
 
 int	main(int ac, char **av)
--- src/server.c
+++ src/server.c
@@ -6,23 +6,40 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 21:03:44 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/15 13:55:03 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/15 15:08:02 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
 static void	server_handler(int signum);
-// static void	end_handler(int signum);
+static int	message_zero(void);
+static void	end_handler(int signum);
+
+char	message[1500];
 
 int	main(void)
 {
 	ft_printf("Server -> process ID <%d>\n", (int)getpid());
 	signal(SIGUSR1, server_handler);
 	signal(SIGUSR2, server_handler);
-//	signal(SIGINT, end_handler);
+	signal(SIGINT, end_handler);
 	while (1)
 		pause();
+	return (0);
+}
+
+static int	message_zero(void)
+{
+	int	i;
+	
+	i = 0;
+	while (message[i] || i < 1500)
+		i++;
+	write (1, &message, i);
+	i = 0;
+	while (i < 1500)
+		message[i++] = 0;
 	return (0);
 }
 
@@ -30,6 +47,7 @@ static void	server_handler(int signum)
 {
 	static int				octet = 8;
 	static unsigned char	c = 0;
+	static int				i = 0;
 
 	c = c << 1;
 	if (signum == SIGUSR1)
@@ -37,17 +55,23 @@ static void	server_handler(int signum)
 	octet--;
 	if (octet == 0)
 	{
-		write(1, &c, 1);
+		if (i < 1500)
+			message[i++] = c;
+		else
+			i = message_zero();
 		if (c == '\0')
+		{
+			i = message_zero();
 			ft_printf("\n[End of message]\n");
+		}
 		octet = 8;
 		c = 0;
 	}
 }
 
-// static void	end_handler(int signum)
-// {
-// 	(void)signum;
-// 	ft_printf("Server terminated\n");
-// 	exit (EXIT_SUCCESS);
-// }
+static void	end_handler(int signum)
+{
+	(void)signum;
+	ft_printf("\n[CTRL + c] Server terminated\n");
+	exit (EXIT_SUCCESS);
+}
