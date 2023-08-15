d6da907b33142a9e22c7850fe71b843225f28942
mvisca <martin.visca@gmail.com>
2023-08-15 13:57:37 +0200

    update

--- src/server.c
+++ src/server.c
@@ -6,21 +6,21 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 21:03:44 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/15 11:58:51 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/15 13:55:03 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
 static void	server_handler(int signum);
-static void	end_handler(int signum);
+// static void	end_handler(int signum);
 
 int	main(void)
 {
 	ft_printf("Server -> process ID <%d>\n", (int)getpid());
 	signal(SIGUSR1, server_handler);
 	signal(SIGUSR2, server_handler);
-	signal(SIGINT, end_handler);
+//	signal(SIGINT, end_handler);
 	while (1)
 		pause();
 	return (0);
@@ -35,20 +35,19 @@ static void	server_handler(int signum)
 	if (signum == SIGUSR1)
 		c = c | 1;
 	octet--;
-	ft_printf("bit %d / octet (%d)\n", c & 1, octet);
 	if (octet == 0)
 	{
+		write(1, &c, 1);
 		if (c == '\0')
-			ft_printf("END\n");
-		ft_printf("c = %c\n", c);
+			ft_printf("\n[End of message]\n");
 		octet = 8;
 		c = 0;
 	}
 }
 
-static void	end_handler(int signum)
-{
-	(void)signum;
-	ft_printf("Server terminated\n");
-	exit (EXIT_SUCCESS);
-}
+// static void	end_handler(int signum)
+// {
+// 	(void)signum;
+// 	ft_printf("Server terminated\n");
+// 	exit (EXIT_SUCCESS);
+// }
