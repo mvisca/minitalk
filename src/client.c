b8f83817d2cb31693588fd80e9aafed58c9b72c2
mvisca <martin.visca@gmail.com>
2023-08-17 01:13:30 +0200

    reduce lines client.c

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:54:35 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/17 00:49:27 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/17 01:02:43 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ static int	mt_clientatoi(const char *nptr)
 static void	mt_send_signal(pid_t pid, char *str)
 {
 	int		octet;
-	int		bit;
 	int		i;
 	char	c;
 
@@ -40,8 +39,7 @@ static void	mt_send_signal(pid_t pid, char *str)
 		{
 			c = str[i];
 			c = c >> octet--;
-			bit = c & 1;
-			if (bit == 1)
+			if (c & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
