a24b912bb97aec8f745dc51a3cd2be15f0f628c6
mvisca <martin.visca@gmail.com>
2023-08-17 00:51:49 +0200

    norminette prep

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:54:35 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/16 17:42:30 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/17 00:49:27 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ static void	mt_send_signal(pid_t pid, char *str)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(400);
+			usleep(300);
 			if (i % 1500 == 0)
 				usleep(4000);
 		}
