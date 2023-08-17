6133f902f219e2cc920643fb33a4a3ffd78571f3
mvisca <martin.visca@gmail.com>
2023-08-18 00:05:49 +0200

    fix .h

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:56:53 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/16 14:04:48 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/18 00:04:25 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,5 @@
 # include <stdio.h>
 # include <unistd.h>
 # include "../libft/include/libft.h"
-
-// Struct to rebuild and store chars
-typedef struct	s_mssg
-{
-	int					current_bit;
-	int					character;
-} t_mssg;
 
 #endif
