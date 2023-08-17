9574f2d6a895e8cba2c6d145c043d7465a9b344f
mvisca <martin.visca@gmail.com>
2023-08-17 23:47:21 +0200

    ok values

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:54:35 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/17 18:49:11 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/17 23:36:58 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ static void	mt_send_signal(pid_t pid, char *str)
 			else
 				kill(pid, SIGUSR2);
 			if (i % 1000 == 0)
-				usleep(2000);
-			usleep(300);
+				usleep(4000);
+			usleep(400);
 		}
 		write (1, "sending... ", 11);
 		write (1, &str[i++], 1);
