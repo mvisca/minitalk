b1f07a7057bf4c5bbfe100607e11665a14d2fdf7
mvisca <martin.visca@gmail.com>
2023-08-17 18:53:44 +0200

    fix conditions order

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:54:35 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/17 01:02:43 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/17 18:49:11 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,13 @@ static void	mt_send_signal(pid_t pid, char *str)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
+			if (i % 1000 == 0)
+				usleep(2000);
 			usleep(300);
-			if (i % 1500 == 0)
-				usleep(4000);
 		}
 		write (1, "sending... ", 11);
-		write (1, &str[i], 1);
+		write (1, &str[i++], 1);
 		write (1, "\n", 1);
-		i++;
 	}
 	octet = 8;
 	while (octet > 0)
@@ -84,6 +83,9 @@ int	main(int ac, char **av)
 		write (2, "sigaction\n", 10);
 	mt_send_signal(pid, av[2]);
 	while (1)
-		pause();
+	{
+		kill(pid, SIGUSR2);
+		usleep(2000);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 21:03:44 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/08/17 00:43:01 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/17 18:44:57 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,137 +14,59 @@
 #include "../libft/include/libft.h"
 #include <signal.h>
 
-static void	server_handler(int signum, siginfo_t *info, void *ctx);
-static int	message_zero(void);
-static void	end_handler(int signum);
+static void	mt_server_handler(int signum, siginfo_t *info, void *ctx);
+static void	mt_print_and_reset_message(unsigned short *i);
 
-char	g_message[1500];
+char	g_message[1000];
 
 int	main(void)
 {
-	struct sigaction	s_end;
 	struct sigaction	s_sa;
 
-	ft_printf("Server PID <%d>\n", (int)getpid());
-	s_end.sa_handler = end_handler;
-	s_sa.sa_sigaction = server_handler;
+	ft_printf("PID: %d\n", (int)getpid());
+	s_sa.sa_sigaction = mt_server_handler;
 	s_sa.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGINT, &s_end, NULL) == -1 || \
-		sigaction(SIGUSR1, &s_sa, NULL) == -1 || \
+	if (sigaction(SIGUSR1, &s_sa, NULL) == -1 || \
 		sigaction(SIGUSR2, &s_sa, NULL) == -1)
-		write (2, "sigaction\n", 10);
+		write (2, "Sigaction\n", 10);
 	while (1)
 		pause();
 	return (0);
 }
 
-static int	message_zero(void)
-{
-	int	i;
-
-	i = 0;
-	while (i < 1500 && g_message[i])
-		i++;
-	write (1, &g_message, i);
-	i = 0;
-	while (i < 1500)
-		g_message[i++] = 0;
-	usleep(500);
-	return (0);
-}
-
-static void	server_handler(int signum, siginfo_t *info, void *ctx)
+static void	mt_server_handler(int signum, siginfo_t *info, void *ctx)
 {
 	static int				octet = 8;
 	static unsigned char	c = 0;
-	static int				i = 0;
+	static unsigned short	i = 0;
 
+	(void)ctx;
 	c = c << 1;
-	if (signum == SIGUSR1 && (ctx || !ctx))
+	if (signum == SIGUSR1)
 		c = c | 1;
-	if (--octet != 0)
-		return ;
-	if (i < 1500)
-		g_message[i++] = c;
-	else
-		i = message_zero();
-	if (c == '\0')
+	octet--;
+	if (octet == 0)
 	{
-		i = message_zero();
-		kill(info->si_pid, SIGUSR1);
-		ft_printf(" \n\n [End of message]\n [Sending confirmation...]\n");
-		ft_printf(" [Confirmation sent]\n");
+		g_message[i++] = c;
+		if (c == 0)
+		{
+			kill(info->si_pid, SIGUSR1);
+			mt_print_and_reset_message(&i);
+			write (1, "\n\n[Process complete]\n", 21);
+			ft_printf("PID: %d\n", (int)getpid());
+		}
+		else if (i == 1000)
+			mt_print_and_reset_message(&i);
+		octet = 8;
+		c = 0;
 	}
-	octet = 8;
-	c = 0;
 }
 
-static void	end_handler(int signum)
+static void	mt_print_and_reset_message(unsigned short *i)
 {
-	(void)signum;
-	ft_printf("\n [CTRL + c] Server terminated\n");
-	exit (EXIT_SUCCESS);
+	write (1, &g_message, *i);
+	*i = 0;
+	while (*i < 1000)
+		g_message[(*i)++] = 0;
+	*i = 0;
 }
-
-// Required code for alternative implementation with linked lists
-// 
-// #include "../include/minitalk.h"
-// 
-// t_char_node *mt_chrlst_new(t_char_node **head, char c)
-// {
-// 	t_char_node *node;
-// 
-// 	node = (t_char_node *) malloc (sizeof(t_char_node));
-// 	if (!node)
-// 	{
-// 		mt_chrlst_clear(head);
-// 		return (NULL);
-// 	}
-// 	node->character = c;
-// 	node->next = NULL;
-// 	return (node);
-// }
-// 
-// void	mt_chrlst_add(t_char_node **head, t_char_node *new)
-// {
-// 	t_char_node	*aux;
-// 
-// 	if (!*head)
-// 	{
-// 		*head = new;
-// 		return ;
-// 	}
-// 	aux = *head;
-// 	while (aux->next)
-// 		aux = aux->next;
-// 	aux->next = new;
-// }
-// 
-// void mt_chrlst_clear(t_char_node **head)
-// {
-// 	t_char_node	*aux;
-// 	t_char_node	*next;
-// 
-// 	aux = head;
-// 	while (aux)
-// 	{
-// 		next = aux->next;
-// 		free(aux);
-// 		aux = next;
-// 	}
-// 	free(head);
-// }
-// 
-// void	mt_print_list(t_char_node *head)
-// {
-// 	char	c;
-// 
-// 	while (head)
-// 	{
-// 		c = head->character;
-// 		write (1, &c, 1);
-// 		head = head->next;
-// 		ft_printf("\naqui\n");
-// 	}
-// 	mt_chrlst_clear(head);
-// }
